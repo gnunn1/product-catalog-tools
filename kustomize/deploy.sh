c6a35a62de086712b14b7b2d1e58ea7bffb7957f
developer <gnunn@redhat.com>
2020-03-16 09:29:38 -0700

    Make promote image conditional

--- kustomize/deploy.sh
+++ kustomize/deploy.sh
@@ -1,11 +1,17 @@
 echo "Creating projects"
-oc new project product-catalog-dev
+oc new-project product-catalog-dev
 oc new-project product-catalog-test
 oc new-project product-catalog-cicd
 
+echo "Adding rolebindings"
+oc apply -k ./security/bases
+
+echo "Deploy nexus"
+oc apply =k ./nexus/bases
+
 echo "Creating product-catalog-dev project"
-kustomize build ./app/overlays/dev --reorder none | oc create -f -
+kustomize build ./app/overlays/dev --reorder none | oc apply -f -
 echo "Creating product-catalog-test project"
-kustomize build ./app/overlays/test --reorder none | oc create -f -
+kustomize build ./app/overlays/test --reorder none | oc apply -f -
 echo "Creating product-catalog-cicd project"
-kustomize build ./cicd/overlays/std --reorder none | oc create -f -
+kustomize build ./cicd/overlays/std --reorder none | oc apply -f -
