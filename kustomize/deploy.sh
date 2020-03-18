0e8aa289fe4c5081208d4df2e4b15b0b05c5463a
developer <gnunn@redhat.com>
2020-03-17 20:01:57 -0700

    Initialize database

--- kustomize/deploy.sh
+++ kustomize/deploy.sh
@@ -9,11 +9,4 @@ oc apply -k ./security/bases
 # echo "Deploy nexus"
 # oc apply -k ./nexus/bases
 
-# echo "Creating product-catalog-dev project"
-# kustomize build ./app/overlays/dev --reorder none | oc apply -f -
-# echo "Creating product-catalog-test project"
-# kustomize build ./app/overlays/test --reorder none | oc apply -f -
-# echo "Creating product-catalog-cicd project"
-# kustomize build ./cicd/overlays/std --reorder none | oc apply -f -
-
 kustomize build ./demo/overlays/rhpds --reorder none | oc apply -f -
