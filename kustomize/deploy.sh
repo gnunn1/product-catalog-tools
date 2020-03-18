d7a32f4fdf5621ce046b0298fc680da537280351
developer <gnunn@redhat.com>
2020-03-17 20:21:31 -0700

    Change to crc overlay

--- kustomize/deploy.sh
+++ kustomize/deploy.sh
@@ -9,4 +9,5 @@ oc apply -k ./security/bases
 # echo "Deploy nexus"
 # oc apply -k ./nexus/bases
 
-kustomize build ./demo/overlays/rhpds --reorder none | oc apply -f -
+# Use the overlay that makes sense, here we are using a Code Ready Containers overlay
+kustomize build ./demo/overlays/crc --reorder none | oc apply -f -
