7d40236ea8d3a375b40a48b1f235b05dbc52fdc4
developer <gnunn@redhat.com>
2020-03-18 14:21:28 -0700

    Rename demo to cluster, roll-in rolebinding as base

--- kustomize/deploy.sh
+++ kustomize/deploy.sh
@@ -1,13 +1,10 @@
-echo "Creating projects"
+# echo "Creating projects"
 oc new-project product-catalog-dev
 oc new-project product-catalog-test
 oc new-project product-catalog-cicd
-
-echo "Adding rolebindings"
-oc apply -k ./security/bases
 
 # echo "Deploy nexus"
 # oc apply -k ./nexus/bases
 
 # Use the overlay that makes sense, here we are using a Code Ready Containers overlay
-kustomize build ./demo/overlays/crc --reorder none | oc apply -f -
+kustomize build ./cluster/overlays/crc --reorder none | oc apply -f -
